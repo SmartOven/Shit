779d870fbd794463d1d5f360f4d93ee87f96b19e
SmartOven <yarik.panteleev@gmail.com>
2020-03-13 03:30:14 +0300

    added things

--- csv_reader.py
+++ csv_reader.py
@@ -1,9 +1,5 @@
 from resources import unique, read_csv, get_check, print_check
 
 tags, data = read_csv("Data.csv")
-# print(tags)
-# print(data)
-# for d in data:
-#     print(d)
 check = get_check(tags, data)
 print_check(check)
