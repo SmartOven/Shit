d3c38424f4b0b1b504c7167e720f301606085bcb
SmartOven <yarik.panteleev@gmail.com>
2020-03-13 03:29:32 +0300

    added some things

--- csv_reader.py
+++ csv_reader.py
@@ -1,10 +1,9 @@
-# import pandas
-from resources import unique, get_tags, read_csv
-# data = pandas.read_csv("Data.csv", ",")
-
-# un = get_tags(data)
-# print(len(un))
+from resources import unique, read_csv, get_check, print_check
 
 tags, data = read_csv("Data.csv")
-print(tags)
-print(data)
+# print(tags)
+# print(data)
+# for d in data:
+#     print(d)
+check = get_check(tags, data)
+print_check(check)
--- resources.py
+++ resources.py
@@ -1,17 +1,15 @@
 def unique(a):
     b = []
     for i in range(len(a)):
-        # Приводим каждый лист к стандартному виду
-        # чтобы [a, b] == [b, a]
-        c = sorted(a[i])
         # Если еще нет такого элемента - добавляем
-        if c not in b:
-            b.append(c)
+        if a[i] not in b:
+            b.append(a[i])
     return b
 
 
 def read_csv(_file_path):
     file = open(_file_path, encoding="utf-8")
+    # Лист тегов из первой строки файла
     _tags = file.readline()
     _tags = list(_tags.split(','))
     # Костыль на первый столбец (ибо он изначально "undefined")
@@ -37,4 +35,29 @@ def read_csv(_file_path):
 
 
 def get_check(_tags, _data):
-    _un = unique()
+    # Копируем лист строк csv
+    _id = list(_data)
+    # Берем из каждой строки только IDПользователя
+    for i in range(len(_id)):
+        _id[i] = _id[i][3]
+    # Из списка IDПользователя выбираем только уникальные
+    _un_id = unique(_id)
+    # Создаем трехмерный список чеков arr[IDПользователя][category][element]
+    _arr = []
+    for i in range(len(_un_id)):
+        _arr.append([])
+    # Обрабатываем каждую строку csv файла
+    for line in _data:
+        # Получаем номер данного IDПользователя в списке уникальных
+        ind = _un_id.index(line[3])
+        # Добавляем всю информацию о покупках пользователя по его IDПользователя в лист
+        _arr[ind].append(list(line[3:]))
+    return _arr
+
+
+def print_check(_check):
+    for a in range(len(_check)):
+        print(_check[a][0][0])
+        print(f"    {len(_check[a])}")
+        for b in _check[a]:
+            print(f"    {b[1:]}")
